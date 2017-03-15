cb06c6fe7bdec449fc30bfe58168cb47e002e692
Ethan Hoewisch <ethan.hoewisch@aeris.net>
2017-03-14 17:57:39 -0700

    This is a mash-up of two sample projects to demonstrate the sending of ...
    ... MQTT messages from the cloud to a device.
    Source projects:
    - https://hub.jazz.net/project/chunbintang/bluemixmqtt/overview
    - https://github.com/IBM-Bluemix/get-started-java
    
    You will need to modify the manifest.yml, cloudant.properties, and app.conf files yourself.
    You will need to add a "deviceid" field to the app.conf file. The value will be the ID of the device whose count you wish to reset.

---
+++ src/main/java/wasdev/sample/rest/DeviceTalkBackAPI.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/ 
+package wasdev.sample.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+
+import com.google.gson.Gson;
+
+import com.ibm.bluemixmqtt.AppTest;
+import com.ibm.bluemixmqtt.AppTestFactory;
+
+@ApplicationPath("api")
+@Path("/devicetalkback")
+public class DeviceTalkBackAPI extends Application {
+	
+	final AppTest appTest = AppTestFactory.getInstance();
+
+    /**
+     * Teslls the device to reset its count
+     * 
+     * REST API example:
+     * <code>
+     * POST http://localhost:9080/GetStartedJava/api/devicetalkback
+     * <code>
+     * POST Body:
+     * <code>
+     * {
+     *   "reset":true
+     * }
+     * </code>
+     * Response:
+     * <code>
+     * "success"
+     * </code>
+     * @param visitor The new Visitor to create.
+     * @return The Visitor after it has been stored.  This will include a unique ID for the Visitor.
+     */
+    @POST
+    @Produces("application/text")
+    @Consumes("application/json")
+    public String newToDo(Map<String,Boolean> visitor) {
+      if(appTest == null) {
+    	  return String.format("Nope, store was null");
+      }
+      appTest.sendReset();
+      return String.format("success");
+
+    }
+
+}
